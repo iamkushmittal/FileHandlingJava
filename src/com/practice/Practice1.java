bab3cca748fed332587a2d991c3b3f640365584c
iamkushmittal <mittalkush1999@gmail.com>
2024-05-03 23:54:47 +0530

    input stream reader

--- src/com/practice/Practice1.java
+++ src/com/practice/Practice1.java
@@ -1,5 +1,23 @@
 package com.practice;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Practice1 {
+	public static void main(String args[]) {
+		
+		try(InputStreamReader isr = new InputStreamReader(System.in)){
+			System.out.print("Enter some letters : ");
+			int letters = isr.read();
+			while(isr.ready()) {
+				System.out.println((char)letters);
+				letters = isr.read();
+			}
+//			isr.close();
+			System.out.println();
+		}
+		catch(IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
 
 }
